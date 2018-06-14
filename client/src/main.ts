7a9779c8dbb01f14d97b5e57f6e03e680ca94fcc
a <b@c.com>
2018-06-14 13:49:20 +0300

    Add scss and css custom properties

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,9 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import cssVars from 'css-vars-ponyfill';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => {
+    cssVars();
+  })
   .catch(err => console.log(err));
